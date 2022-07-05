af57e5bcf6a2f4023999d9131ba4cd03987b4e64
Ian Dao <ibdao921@gmail.com>
2022-07-05 16:46:13 -0700

    created schema for shipment

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const router = new express.Router();
-
+const jsonschema = require("jsonschema");
+const shipSchema = require("../schemas/shipSchema.json");
 const { shipProduct } = require("../shipItApi");
+const { BadRequestError } = require("../expressError")
 
-/** POST /ship
+/** POST /
  *
  * VShips an order coming from json body:
  *   { productId, name, addr, zip }
@@ -14,8 +16,16 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
+  const result = jsonschema.validate(req.body, shipSchema, {required: true});
+
+  if(!result.valid){
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
+
   return res.json({ shipped: shipId });
 });
 
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -3,6 +3,12 @@
 const request = require("supertest");
 const app = require("../app");
 
+/** Tests adding a shipment
+ * productId must be >= 1000, name, addr, zipcode are strings 
+ * All are required
+ * Returns {shipped: shipId} on success
+ * Returns {error:{(info about error)}} on failure
+*/
 
 describe("POST /", function () {
   test("valid", async function () {
@@ -10,9 +16,20 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zip: "12345-6789",
+      zipcode: "12345-6789",
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("invalid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zipcode: "12345-6789",
+    });
+
+    expect(resp.body.error.status).toEqual(400);
   });
 });
