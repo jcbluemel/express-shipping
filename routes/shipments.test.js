2a95ef315bd7fcae0c099d2d783ee60c59276c83
Joel <jcbluemel@yahoo.com>
2022-07-05 17:28:01 -0700

    added mock for shipProduct in shipments.test

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,10 +1,30 @@
 "use strict";
+const shipItApi = require("../shipItApi");
+shipItApi.shipProduct = jest.fn();
+shipItApi.shipProduct.mockReturnValue(5555);
 
 const request = require("supertest");
 const app = require("../app");
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
+// const { shipProduct, SHIPIT_SHIP_URL, SHIPIT_API_KEY } = require("./shipItApi");
+
+
+
+// axiosMock.onPost(`${SHIPIT_SHIP_URL}`, {
+//   itemId: 2000,
+//   name: "test",
+//   addr: "11 test address",
+//   zip: "55555",
+//   key: SHIPIT_API_KEY
+// }).reply(201, 5000);
+
+
 
 /** Tests adding a shipment
- * productId must be >= 1000, name, addr, zipcode are strings 
+ * productId must be >= 1000, name, addr, zipcode are strings
  * All are required
  * Returns {shipped: shipId} on success
  * Returns {error:{(info about error)}} on failure
@@ -19,7 +39,7 @@ describe("POST /", function () {
       zipcode: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 5555 });
   });
 
   test("invalid", async function () {
@@ -29,7 +49,7 @@ describe("POST /", function () {
       addr: "100 Test St",
       zipcode: "12345-6789",
     });
-    //TODO: strengthen expectations for invalid test. 
+    //TODO: strengthen expectations for invalid test.
     expect(resp.body.error.status).toEqual(400);
   });
 });
